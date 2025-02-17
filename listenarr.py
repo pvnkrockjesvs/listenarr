da8d6acae0398eef481dd1300aa1812e2d473d9e
Guillaume Ronnel <guillaume@rogSUSE.home>
2025-02-17 17:24:02 +0100

    first commit

---
+++ listenarr.py
@@ -0,0 +1,83 @@
+import requests
+import json
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+def get_excluded_artists(lidarr_url, api_key):
+    headers = {
+        'X-Api-Key': api_key,
+    }
+    response = requests.get(f'{lidarr_url}/api/v1/importlistexclusion', headers=headers)
+    if response.status_code == 200:
+        return [exclusion['foreignId'] for exclusion in response.json()]
+    else:
+        print(f"Failed to fetch exclusion list. Status code: {response.status_code}")
+        return []
+
+def add_artist_to_lidarr(lidarr_url, api_key, mbid, artist_name, root_folder, excluded_artists):
+    if mbid in excluded_artists:
+        print(f"Skipping excluded artist: {artist_name} (MBID: {mbid})")
+        return
+
+    headers = {
+        'X-Api-Key': api_key,
+        'Content-Type': 'application/json'
+    }
+
+    payload = {
+        'foreignArtistId': mbid,
+        'artistName': artist_name,
+        'rootFolderPath': root_folder,
+        'monitored': True,
+        'qualityProfileId': 1,
+        'metadataProfileId': 1,
+        "addOptions": {
+            "searchForMissingAlbums": False
+        },
+    }
+
+    response = requests.post(f'{lidarr_url}/api/v1/artist', headers=headers, json=payload)
+
+    if response.status_code == 201:
+        print(f"Successfully added artist: {artist_name} (MBID: {mbid})")
+    # else:
+    #     print(f"Failed to add artist: {artist_name} (MBID: {mbid}). Status code: {response.status_code}")
+    #     print(f"Response content: {response.text}")
+
+def get_top_artists(username, range, count, min_listen):
+    url = f"https://api.listenbrainz.org/1/stats/user/{username}/artists"
+    params = {
+        'range': range,
+        'count': count
+    }
+
+    response = requests.get(url, params=params)
+    response.raise_for_status()  # Raise an exception for HTTP errors
+
+    data = response.json()
+    artists = data['payload']['artists']
+
+    return [artist for artist in artists if artist['listen_count'] > min_listen]
+
+# Configuration
+lidarr_url = os.getenv("URL")
+api_key = os.getenv("API")
+root_folder = os.getenv("ROOT_FOLDER")
+username = os.getenv("USERNAME")
+range = 'week'
+count = 150
+min_listen = 5
+
+
+
+# Get excluded artists
+excluded_artists = get_excluded_artists(lidarr_url, api_key)
+
+# Get top artists (assuming this function is defined elsewhere)
+artists = get_top_artists(username, range, count, min_listen)
+
+# Add artists
+for artist in artists:
+    add_artist_to_lidarr(lidarr_url, api_key, artist['artist_mbid'], artist['artist_name'], root_folder, excluded_artists)
